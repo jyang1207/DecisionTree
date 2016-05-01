b4c52de81b492518b673fe5047074853e19fac1e
jyang1207 <jyang@colby.edu>
2016-05-01 18:43:51 -0400

    Update forest.py

--- forest.py
+++ forest.py
@@ -3,9 +3,16 @@ importy numpy as np
 
 class forest:
 	
-	def __init__(self, trees=[]):
+	def __init__(self, dataObj=None, categories=None, trees=[]):
 		self.trees = trees
-		
-	def build(self, numTrees):
+		self.dataObj = dataObj
+		self.categories = categories
+	
+	#build a forest with given number and depth of trees	
+	def build(self, train_data, numTrees, depth, z):
+		for i in range(numTrees):
+			tree = tree.Tree(self.dataObj, self.categories, depth)
+			tree.build(train_data, self.categories, weights, depth, z)
+			self.trees.append(tree)
 		
 	def classify(self, dataMatrix):
