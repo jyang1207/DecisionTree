e24cda67c861ca0bf47adf1c99f89f0a3a5a6529
jyang1207 <jyang@colby.edu>
2016-05-02 18:16:22 -0400

    Update forest.py

--- forest.py
+++ forest.py
@@ -5,21 +5,21 @@ class forest:
 	
 	def __init__(self, dataObj=None, categories=None, trees=[], z=0):
 		self.trees = trees
-		
+		self.dataObj = dataObj
 		if self.dataObj != None:
 			self.build(dataObj, categories, depth, z)
 	
-	#build a forest with given number and depth of trees	
+	#use Adaboost to build a forest with given depth of trees	
 	def build(self, train_data, categories, depth, z):
-		if train_data.shape[0] != categories.shape[0]:
+		data_size = train_data.get_raw_num_rows()
+		if data_size != categories.shape[0]:
 			print "training data and categories have different sizes."
 			return
-		weights = np.ones_like(self.categories)
-		correctCount = train_data.shape[0]
-		while correctCount > 0.5*train_data.shape[0]:
+		weights = np.ones_like(categories)
+		correctCount = data_size
+		while correctCount > 0.5*data_size:
 			correctCount = 0
-			tree = tree.Tree(self.dataObj, self.categories, depth)
-			tree.build(train_data, self.categories, weights, depth, z)
+			tree = tree.Tree(train_data, categories, depth, z)
 			tree.prune()
 			self.trees.append(tree)
 			cats = tree.classify(train_data)
@@ -30,8 +30,15 @@ class forest:
 					correctCount += 1
 				else:
 					weights[i] *= 1.25
-		
+	
+	#classify a given data set and return a matrix of categories	
 	def classify(self, dataMatrix):
-		cats = 
-		for tree in self.trees:
-			
+		cats = np.matrix(np.zeros(shape=(dataMatrix.shape[0], 1)))
+		unique, mapping = np.unique(np.array(categories), return_inverse = True)
+		forest_cats = np.matrix(np.zeros(shape=(dataMatrix.shape[0], len(self.trees)))
+		votes = []
+		for i in range(len(unique)):
+			votes.append(0)
+		for t in range(len(self.trees)):
+			tree_cats = self.trees[t].classify(dataMatrix)
+			forest_cats[:, t] = tree_cats
