a3bd88ddcb80844184d75a6a7aba66b0b219a3f4
jyang1207 <jyang@colby.edu>
2016-05-02 17:46:11 -0400

    Update forest.py

--- forest.py
+++ forest.py
@@ -3,25 +3,31 @@ importy numpy as np
 
 class forest:
 	
-	def __init__(self, dataObj=None, categories=None, trees=[]):
+	def __init__(self, dataObj=None, categories=None, trees=[], z=0):
 		self.trees = trees
-		self.dataObj = dataObj
-		self.categories = categories
+		
+		if self.dataObj != None:
+			self.build(dataObj, categories, depth, z)
 	
 	#build a forest with given number and depth of trees	
-	def build(self, train_data, numTrees, depth, z):
-		weights = np.ones_like(categories)
-		for i in range(numTrees):
+	def build(self, train_data, categories, depth, z):
+		if train_data.shape[0] != categories.shape[0]:
+			print "training data and categories have different sizes."
+			return
+		weights = np.ones_like(self.categories)
+		correctCount = train_data.shape[0]
+		while correctCount > 0.5*train_data.shape[0]:
+			correctCount = 0
 			tree = tree.Tree(self.dataObj, self.categories, depth)
-			#calculate weight
-			
 			tree.build(train_data, self.categories, weights, depth, z)
 			tree.prune()
 			self.trees.append(tree)
 			cats = tree.classify(train_data)
+			#increase the weight of data points that the previous tree classifies incorrectly
 			for j in range(len(weights)):
 				if cats[i] == self.categories:
-					weights[i] *= -1.25
+					weights[i] *= 0.75
+					correctCount += 1
 				else:
 					weights[i] *= 1.25
 		
