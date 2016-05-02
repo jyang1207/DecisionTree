a148152ddc6d601ae9d95d632bc4843561d989b3
jasongurv <jmgurevi@colby.edu>
2016-05-02 09:11:41 -0400

    Update forest.py
    added a formula to reweigh after each tree

--- forest.py
+++ forest.py
@@ -10,6 +10,7 @@ class forest:
 	
 	#build a forest with given number and depth of trees	
 	def build(self, train_data, numTrees, depth, z):
+		weights = np.ones_like(categories)
 		for i in range(numTrees):
 			tree = tree.Tree(self.dataObj, self.categories, depth)
 			#calculate weight
@@ -17,6 +18,12 @@ class forest:
 			tree.build(train_data, self.categories, weights, depth, z)
 			tree.prune()
 			self.trees.append(tree)
+			cats = tree.classify(train_data)
+			for j in range(len(weights)):
+				if cats[i] == self.categories:
+					weights[i] -= 0.25
+				else:
+					weights[i] += 0.25
 		
 	def classify(self, dataMatrix):
 		cats = 
